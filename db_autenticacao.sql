45ede2095517310016b022749aa6ca0844c69493
Ianniely <138123066+Ianniely@users.noreply.github.com>
2023-10-18 17:50:51 -0300

    Update db_autenticacao.sql

--- db_autenticacao.sql
+++ db_autenticacao.sql
@@ -10,3 +10,9 @@ CREATE TABLE usuarios (
     cidade VARCHAR(255) NOT NULL,
     estado VARCHAR(255) NOT NULL
 );
+
+INSERT INTO usuarios (nome, email, senha, telefone, cidade, estado)
+VALUES 
+    ('John Doe', 'john.doe@example.com', 'password123', '555-555-5555', 'Cityville', 'Stateville'),
+    ('Jane Smith', 'jane.smith@example.com', 'password456', '555-555-5555', 'Townsville', 'Stateland'),
+    ('Bob Johnson', 'bob.johnson@example.com', 'password789', '555-555-5555', 'Villageville', 'Statecity');
